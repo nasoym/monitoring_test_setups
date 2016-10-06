920039a8054288f228f3d6426dc90be1c286f8fd
Sinan Goo <sinan.goo@hypoport.de>
2016-10-06 12:39:54 +0200

    Added annotations

--- grafana_json_data_source/run.sh
+++ grafana_json_data_source/run.sh
@@ -2,10 +2,17 @@
 
 docker-compose up -d
 
+echo -n "waiting for grafana to be ready: "
 for i in {1..20}; do 
   curl -s "http://localhost:3000" >/dev/null && break
+  echo -n "."
   sleep 1
 done
+echo
 curl -s "http://localhost:3000" >/dev/null || { echo "grafana did not respond!" >&2; exit 1;}
+echo "adding datasource to grafna"
 ./add_datasources
+echo
+
+open -a "Google Chrome" "http://localhost:3000/dashboard/file/grafana_dashboard.json"
 
